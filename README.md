24cc73d726e7c9430fb4f04bb22c5da554f8db57
prathameshap <prathameshpawar1301@gmail.com>
2020-05-06 12:22:36 -0700

    Updated all dockstrings

--- README.md
+++
@@ -1,2 +0,0 @@
-# Matrix-Operations-class
-matrix_class in Python to support initialize and arithmatic operations
--- matrix.py
+++ matrix.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
-"""This new branch is created so that everyone can make changes to this file"""
+
+#Filename: matrix.py
 # Author - Prathamesh Pawar 
 # Email  - prathameshpawar1301@gmail.com
 
@@ -16,9 +17,13 @@
 
 class Matrix:
     
-    """Creates object of  class Matrix with number of rows and columns"""
+
     def __init__(self, *args):
-        
+        """__init__(*args) --> initializes matrix object with given parameters(rows and columns)
+        Parameters:
+        arg0: (if  only 1 arg is given then rows and columns) number of rows
+        arg1: number of columns
+        """
         numargs = len(args)
 
         if numargs < 1:
@@ -34,14 +39,22 @@ class Matrix:
             raise TypeError(f'At most 2 arguments ara permitted, but got{numargs}')
         self.initMatrix(0)
             
-    """Initializes matrix with given int val and return int list of list"""
     def initMatrix(self, val:int)->int:
+        """initMatrix(val:int)--> initializes nxm matrix to given value
+        Parameters:
+        val: value to initialize matrix with 
+        """
         self._matrix = [[val for x in range(self._columns)] for y in range(self._rows)]
         return self._matrix
 
-    """Changes the value of perticular position in matrix"""
-
     def changeElements(self, **kwargs):
+        """changeElements(rw_chng= int, cl_chng = int, val_chng = int) --> changas single value if row and
+        column are given or, Either changes entire row or entire column
+        Parameters:
+        rw_chng = gives row to change
+        cl_chng = gives column to change
+        val_chng = gives value to change to 
+        """
         rw_chng  = None
         cl_chng  = None
         val_chng = None
@@ -69,9 +82,12 @@ class Matrix:
 
         return self._matrix
 
-    """Multiply matrix by a scaler value"""
 
     def scalerMultiplier(self, scal_val):
+        """scalerMultiplier(scal_val: int)-->multiplies each element of the matrix by given scalar value
+        Paramater:
+        scal_val = multiplies entire matrix(each individual value of the matrix) by this value
+        """
 
         if scal_val != None:
 
@@ -83,9 +99,11 @@ class Matrix:
         else:
             raise TypeError(f'Need scaler value to multiply')
 
-    """Add perticular scaler value to matrix"""
-
     def scalerAddition(self, scal_val):
+        """scalarAddition(scal_val:int)--> adds given scalar value with each elements of the matrix
+        Paramaters:
+        scal_val = Adds each value of the matrix by this scalar value
+        """
 
         if scal_val != None and scal_val != 0:
 
@@ -99,15 +117,18 @@ class Matrix:
         else:
             raise TypeError(f'Need scaler value to Add')
 
-    """Print the resulting Matrix"""
-
     def printMatrix(self):
+        """printMatrix()--> prints matrix in matrix form
+        No Parameters
+        """
         for row in self._matrix:
             print(row)
 
-    """Finding Transpose of the matrix"""
 
     def findTranspose(self):
+        """findTranspose()--> gives transpose of the current matrix object 
+        No Parameters
+        """
 
         tr_matrix = [[self._matrix[j][i] for j in range(len(self._matrix))]for i in range(len(self._matrix[0]))]
 
